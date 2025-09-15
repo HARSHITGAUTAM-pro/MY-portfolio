01ba1a25cabd94688723d56e52e07e0dd6b25cb9
Harshit Gautam <harshitgautam1984@gmail.com>
2025-09-16 01:20:00 +0530

    Add files via upload

---
+++ demo1.js
@@ -0,0 +1,11 @@
+// function demo(){
+//     const divEl = document.querySelectorAll(".forEachFunction p");
+//     divEl.forEach(item => {
+//         item.addEventListener("mouseover", () => {
+//               item.style.color = "red"
+//         })
+//         // item.textContent = "";
+//         // item.style.fontSize = "120px"
+//     })
+// }
+// demo();
